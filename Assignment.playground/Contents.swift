4df02162309400ddc16ae492a58860f38418e68f
cenkgoktay <cenkgoktay@gmail.com>
2022-09-22 01:39:59 +0300

    Initial Commit

---
+++ Assignment.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+var islemNo : Int = 263752
+var ogrNo : Int = 510
+var kitapNo : Int = 55910
+var aTarih : String = "2022-05-12"
+var vTarih : String = "2022-06-17"
+var ogrAd : String = "Cenk"
+var ogrSoyad : String = "Goktay"
+var cinsiyet : String = "Erkek"
+var dTarih : String = "1995-03-12"
+var sinif : String = "12-E"
+var isbnNo : Int = 83
+var kitapAdi : String = "LOTR"
+var yazarNo : Int = 1
+var turNo : Int = 100
+var sayfaSayisi : Int = 500
+var puan : Int = 90
+var yazarAd : String = "John Ronald Reuel"
+var yazarSoyad : String = "Tolkien"
+var turAdi : String = "Fantastik Kurgu"
+
